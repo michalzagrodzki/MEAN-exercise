296214bc0cbc53728ed9139fc2393a7c00f18d85
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-09-08 20:32:10 +0200

    Add angular routes.

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -1,3 +1,19 @@
-/**
- * Created by michal on 2015-09-08.
- */
+angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+
+  $routeProvider
+
+    // home page
+    .when('/', {
+      templateUrl: 'views/home.html',
+      controller: 'MainController'
+    })
+
+    // nerds page that will use the NerdController
+    .when('/nerds', {
+      templateUrl: 'views/nerd.html',
+      controller: 'NerdController'
+    });
+
+  $locationProvider.html5Mode(true);
+
+}]);
